77491f2f96436af398eae57eb35ac8f842aaf530
Amber King <amber.king9292@gmail.com>
2023-01-18 12:06:28 -0500

    format of HTML and CSS files and connected to main.js file

--- main.js
+++ main.js
@@ -7,11 +7,17 @@
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+    // Add code you want to run on page load here
+    const baseURL = `https://resource-ghibli-api.onrender.com/`
+    fetch(`${baseURL}`)
+    .then(()=>())
+    .then(()=>{
+
+    })
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
 // A non-hacky solution is being researched
-
+//do not delete --v
 setTimeout(run, 1000);
